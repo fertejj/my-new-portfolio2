550c45168415e7df97a7d80d7d2c83e1a579bc74
fertejj <fernandojtejerina@gmail.com>
2025-01-19 18:52:53 -0300

    Se agrega form Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About/About"
+import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
 import Hero from "./components/Hero/Hero"
 import Projects from "./components/Projects/Projects"
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
       <Footer />
     </>
   )
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,46 @@
+import React, {useRef} from 'react'
+import "./Contact.css"
+import {MdEmail} from 'react-icons/md'
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wnsvo1v', 'template_nmz7jfd', form.current, 'PVIz0U5bqjOb3e1kn')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <section id='contact'>
+      <h5>Acercate!</h5>
+      <h2>Escribime</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>fernandojtejerina@gmail.com</h5>
+            <a href="mailto:fernandojtejerina@gmail.com">Escribir correo</a>
+          </article>
+        </div>
+      {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <p>Mandame un mail directamente desde aqui si lo deseas:</p>
+          <input type="text" name='name' placeholder='Nombre completo' required/>
+          <input type="text" name='email' placeholder='Email' required/>
+          <textarea name="message" rows="7" placeholder='Mensaje...' required></textarea>
+          <button type='submit' className='btn btn-primary'>Enviar</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
